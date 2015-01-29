08746745af7a3bca95afa3aac9fec6553e64ae99
Luke Bond <luke.n.bond@gmail.com>
2015-01-29 18:35:11 +0100

    Problem: Node.js RFC2 implementation missing

---
+++ node-rfc2/client/client.js
@@ -0,0 +1,70 @@
+var async = require('async');
+var zmq = require('zmq');
+
+var TOPIC = 'discovery';
+var PUB_PORT = '6665';
+var SUBNET = '192.168.1';
+var IP_LAST_OCTET = '39';
+var HOST = SUBNET + '.' + IP_LAST_OCTET;
+
+function makeRequest(octetAndPort) {
+  var address = 'tcp://' + SUBNET + '.' + octetAndPort;
+  console.log('Client will make request to ' + address);
+  var dealer = zmq.socket('dealer');
+  dealer.identity = HOST + '/' + process.pid;
+  dealer.connect(address);
+  dealer
+    .on('message', function onDealerMessage(message) {
+      console.log('Client received response ' + message.toString());
+    })
+    .on('error', function onDealerError(err) {
+      console.log('Client received error ' + err.toString());
+    });
+  dealer.send('Hello');
+}
+
+function connect(host, port, cb) {
+  var socket = zmq.socket('sub');
+  socket.identity = new Buffer(HOST + '/' + process.pid, 'utf8');
+  var connstr = 'tcp://' + host + ':' + port;
+  console.log('Trying ' + connstr + '...');
+  socket.connect(connstr);
+  socket
+    .on('message', function (topic, message) {
+      console.log('Subscription result: ' + message.toString());
+      if (!topic) {
+        console.log('Error: Topic missing!');
+      } else if (!message) {
+        console.log('Error: Message missing!');
+      } else {
+        var serverAddr = message.toString();
+        console.log('Discovered ' + serverAddr + ' from ' + connstr);
+        makeRequest(serverAddr);
+      }
+      socket.close();
+      return cb(null);
+    })
+    .on('error', function (err) {
+      console.log('Got error from ' + connstr);
+      socket.close();
+      return cb(err);
+    });
+  socket.subscribe(TOPIC);
+}
+
+var addrs = [];
+for (var i = 0; i < 256; ++i) {
+  var ip = SUBNET + '.' + i;
+  addrs[i] = ip;
+}
+var jobs = [];
+addrs.forEach(function (host) {
+  jobs.push(connect.bind(null, host, PUB_PORT));
+});
+async.parallel(jobs, function (err) {
+  if (err) {
+    console.log('Got error: ' + err.toString());
+  } else {
+    console.log('got no error');
+  }
+});
---
+++ node-rfc2/server/server.js
@@ -0,0 +1,42 @@
+var zmq = require('zmq');
+
+var SECRET_STRING = '@lukeb0nd';
+var ERROR_STRING = 'Error';
+var IP_LAST_OCTET = '39';
+var HOST = '192.168.1.' + IP_LAST_OCTET;
+var PUB_PORT = '6665';
+var ROUTER_PORT = '26666';
+var TOPIC = 'discovery';
+var PUB_ADDR = 'tcp://' + HOST + ':' + PUB_PORT;
+var ROUTER_ADDR = 'tcp://' + HOST + ':' + ROUTER_PORT;
+var PUBLISH_STRING = IP_LAST_OCTET + ':' + ROUTER_PORT;
+
+var routerSocket = zmq.socket('router');
+routerSocket
+  .bindSync(ROUTER_ADDR)
+  .on('message', function onRouterMessage(envelope, data) {
+    var message = data.toString();
+    console.log('onRouterMessage: received ' + envelope + ' - ' + message);
+    if (message === 'Hello') {
+      routerSocket.send([envelope, SECRET_STRING]);
+    } else {
+      routerSocket.send([envelope, ERROR_STRING]);
+    }
+  })
+  .on('error', function onRouterError(err) {
+    console.log('onRouterError ' + err.toString());
+  });
+
+var pubSocket = zmq.socket('pub');
+
+function announce() {
+  console.log('Broadcasting ' + PUBLISH_STRING);
+  pubSocket.send([TOPIC, PUBLISH_STRING]);
+}
+
+pubSocket
+  .bindSync(PUB_ADDR)
+  .on('error', function onPubError(err) {
+    console.log('onPubError ' + err.toString());
+  });
+setInterval(announce, 1000);

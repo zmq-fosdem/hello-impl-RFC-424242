2d68f305d1f5d288ea5146caa215777c5bcc8aeb
Brian Knox <bknox@digitalocean.com>
2015-01-29 17:42:07 +0100

    go client done

--- go/cmd/client/client.go
+++ go/cmd/client/client.go
@@ -8,19 +8,53 @@ import (
 )
 
 func main() {
-	var endpoints []string
-	for i := 1; i < 255; i++ {
-		endpoints = append(endpoints, fmt.Sprintf("tcp://192.168.1.%d:6666", i))
-	}
+	tellme := make(chan []byte)
 
-	list := strings.Join(endpoints, ",")
-	s, _ := goczmq.NewDEALER(list)
-	for i := 0; i < 1000; i++ {
-		s.SendFrame([]byte("Hello"), 0)
-	}
+	go func() {
+		var endpoints []string
+		for i := 1; i < 255; i++ {
+			endpoints = append(endpoints, fmt.Sprintf("tcp://192.168.1.%d:6665", i))
+		}
 
-	for i := 0; i < 255; i++ {
-		msg, _, _ := s.RecvFrame()
-		fmt.Printf("%v\n", string(msg))
+		list := strings.Join(endpoints, ",")
+		s, _ := goczmq.NewSUB(list, "")
+		for {
+			advert, _, err := s.RecvFrame()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("advert: %s\n", advert)
+			tellme <- advert
+		}
+	}()
+
+	for {
+		advert := <-tellme
+		dealer := goczmq.NewSock(goczmq.DEALER)
+		endpoint := fmt.Sprintf("tcp://192.168.1.%s", string(advert))
+		fmt.Printf("connecting to: %s\n", endpoint)
+		err := dealer.Connect(endpoint)
+		if err != nil {
+			continue
+		}
+		err = dealer.SendFrame([]byte("Hello"), 0)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		poller, _ := goczmq.NewPoller(dealer)
+		defer poller.Destroy()
+		s := poller.Wait(5000)
+		if s != nil {
+			reply, _, err := s.RecvFrame()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			fmt.Printf("received: %s\n", string(reply))
+		}
+		dealer.Destroy()
 	}
 }

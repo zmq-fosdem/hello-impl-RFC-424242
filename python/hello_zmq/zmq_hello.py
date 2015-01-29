003162043dea071c404c51260c7c11e8140d2d75
Vladimir Lushnikov <vladimir@slate-project.org>
2015-01-29 17:21:26 +0000

    implement the second rfc

--- python/hello_zmq/zmq_hello.py
+++ python/hello_zmq/zmq_hello.py
@@ -4,47 +4,103 @@
 A hello world example using ZMQ
 
 Usage:
-    zmq_hello [options] server
-    zmq_hello [options] client <remote_addr>
+    zmq_hello [options] server <secret> <last_octet>
+    zmq_hello [options] client
 
 Options:
-    -h --help           Show this help menu
+    -h --help                   Show this help menu
 
-    --port=<port>       Port to bind to/connect to [default: 6666]
+    --pub-port=<pub-port>       Port to publish discovery information on [default: 6665]
+    --network=<network>         Network to connect to [default: 192.168.1.0/24]
 """
+import ipaddress
+import logging
+from threading import Thread
+from time import sleep
 from docopt import docopt
 import sys
 import zmq
 
+log = logging.getLogger(__name__)
+
 
 def _client(opts):
-    port_num = int(opts['--port'])
-    remote_addr = opts['<remote_addr>']
+    pub_port = int(opts['--pub-port'])
+    network = opts['--network']
+
 
     ctx = zmq.Context()
-    socket = ctx.socket(zmq.REQ)
-    socket.connect('tcp://{}:{}'.format(remote_addr, port_num))
+    sub_socket = ctx.socket(zmq.SUB)
+    sub_socket.setsockopt(zmq.SUBSCRIBE, b"")
 
-    msg = 'Hello'
-    socket.send_string(msg)
-    reply = socket.recv_string()
+    # connect to all the addrs
+    for host in ipaddress.ip_network(network).hosts():
+        sub_socket.connect('tcp://{}:{}'.format(host, pub_port))
 
-    print(reply)
+    # fixme this is a hack
+    network_prefix = '.'.join(ipaddress.ip_network(network).exploded.split('.')[:-1])
+
+    while True:
+        msg = sub_socket.recv_string()
+        try:
+            last_octet, port = msg.split(':')
+            last_octet = int(last_octet)
+            port = int(port)
+            client_addr = '{}.{}'.format(network_prefix, last_octet)
+            print('trying to connect to {}:{}'.format(client_addr, port))
+
+            dealer_socket = ctx.socket(zmq.DEALER)
+            dealer_socket.setsockopt(zmq.RCVTIMEO, 1000)
+            endpoint = 'tcp://{}:{}'.format(client_addr, port)
+            dealer_socket.connect(endpoint)
+
+            dealer_socket.send_string('Hello')
+            reply = dealer_socket.recv_string()
+            print('Got reply {!r} from dealer endpoint {}'.format(reply, endpoint))
+            dealer_socket.close()
+        except ValueError:
+            pass
+        except zmq.Again:
+            log.error('Client unavailable!')
+        except Exception:
+            log.error('Eek', exc_info=True)
 
 
 def _server(opts):
-    port_num = int(opts['--port'])
+    pub_port = int(opts['--pub-port'])
 
+    # FIXME - get the last octet discovered
+    last_octet = int(opts['<last_octet>'])
+    secret = opts['<secret>']
+
+    # start the pub socket
     ctx = zmq.Context()
-    socket = ctx.socket(zmq.REP)
-    socket.bind('tcp://*:{}'.format(port_num))
+    pub_sock = ctx.socket(zmq.PUB)
+    pub_sock.bind('tcp://*:{}'.format(pub_port))
+
+    router_sock = ctx.socket(zmq.ROUTER)
+    router_port = router_sock.bind_to_random_port('tcp://*')
+
+    print("Hello router will listen on {}".format(router_port))
+
+    def server_hello():
+        while True:
+            try:
+                client_id, msg = router_sock.recv_multipart()
+                msg = msg.decode('ascii')
+                if msg == 'Hello':
+                    router_sock.send_multipart([client_id, secret.encode('ascii')])
+                else:
+                    router_sock.send_string([client_id, 'Error'.encode('ascii')])
+            except UnicodeDecodeError:
+                log.error('Error decoding', exc_info=True)
+
+    router_thread = Thread(target=server_hello)
+    router_thread.start()
 
     while True:
-        msg = socket.recv_string()
-        if msg == 'Hello':
-            socket.send_string('Hello world')
-        else:
-            socket.send_string('Error')
+        pub_sock.send_string('{}:{}'.format(last_octet, router_port))
+        sleep(1)
 
 
 def main(opts):
@@ -56,4 +112,5 @@ def main(opts):
         raise ValueError("Unknown top-level command")
 
 if __name__ == '__main__':
+    logging.basicConfig()
     sys.exit(main(docopt(__doc__)))

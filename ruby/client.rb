f3b53b58e224676e36b61dbf2cb6c8e4e48c2892
Florian Ebeling <florian.ebeling@gmail.com>
2015-02-02 20:56:25 +0100

    Some cleanup

--- ruby/client.rb
+++ ruby/client.rb
@@ -4,17 +4,19 @@ def query(context, address, port)
 
   dealer = context.socket(ZMQ::DEALER)
   server = "tcp://192.168.1.#{address}:#{port}"
-  puts "query ... hello server address: #{server}"
+  puts "Query hello server at: #{server}"
 
   dealer.setsockopt(ZMQ::RCVTIMEO, 5_000)
   dealer.setsockopt(ZMQ::SNDTIMEO, 5_000)
   dealer.connect(server)
 
-  dealer.send_string "Hello"
+  message = "Hello"
+  puts "#{server} < #{message.inspect}"
+  dealer.send_string message
 
-  msgs = []
-  dealer.recv_strings(msgs)
-  puts "#{server} said: '#{msgs.inspect}'"
+  msg = ""
+  dealer.recv_string(msg)
+  puts "#{server} > '#{msg}'"
   dealer.close
 
 end
@@ -27,29 +29,21 @@ socket.setsockopt(ZMQ::RCVTIMEO, 5_000)
 
 pattern = /^(\d{1,3}):(\d{1,5})$/
 
+for ip in [9, 25]
+  address = "tcp://192.168.1.#{ip}:6665"
+  rc = socket.connect(address)
+end
+
 while true
-  for ip in [9]
+  msg = ''
+  rc = socket.recv_string(msg)
 
-    address = "tcp://192.168.1.#{ip}:6665"
-    rc      = socket.connect(address)
-    puts "subscribed #{address}: OK"
-
-    msg = ''
-    rc = socket.recv_string(msg)
-    puts "received: #{msg.to_s} #{rc.inspect}"
-
-    if msg.to_i != -1 && msg.to_s.size > 0
-      result = pattern.match(msg.to_s)
-      ok = if result
-             "ok (address: #{result[1]}, port: #{result[2]})"
-           else
-             "nope"
-           end
-      puts "discovered: #{msg.to_s} : #{ok}"
-      query(context, result[1], result[2]) if result
-    else
-      puts "error msg: #{msg.inspect}"
-    end
-
+  if msg.to_i != -1 && msg.to_s.size > 0
+    result = pattern.match(msg.to_s)
+    puts "Service discovered: #{msg.inspect}"
+    query(context, result[1], result[2]) if result
+  else
+    puts "Error msg: #{msg.inspect}"
   end
+
 end
--- ruby/server.rb
+++ ruby/server.rb
@@ -1,6 +1,7 @@
 require 'ffi-rzmq'
 require 'socket'
 
+nickname   = ARGV[0] || "@febeling"
 port       = 6666
 ip         = Socket.ip_address_list.find { |intf| intf.ipv4_private? }
 last_octet = ip.ip_address.split(".").last
@@ -10,45 +11,43 @@ hello_server = Thread.new do
 
   context = ZMQ::Context.new
   socket = context.socket(ZMQ::ROUTER)
-  socket.setsockopt(ZMQ::RCVTIMEO, 10_000)
+  socket.setsockopt(ZMQ::RCVTIMEO, 30_000)
   socket.bind("tcp://0.0.0.0:#{port}")
 
   while true
-    puts "hello loop"
-
     msgs = []
     socket.recv_strings msgs
-    puts "hello server received: #{msgs}"
+    sender, payload = msgs
+    puts "> #{payload.inspect}"
 
-    if pattern.match(msgs.last)
-      puts "."
-      rc = socket.send_strings [msgs.first, "Cat bus"]
-      puts "sending hello answer: #{rc.inspect}"
+    if pattern.match(payload)
+      answer = "Hello from #{nickname}"
+      puts "< #{answer.inspect}"
+      rc = socket.send_strings [sender, answer]
     else
-      puts "error: #{msgs.inspect}"
+      puts "Protocol error: #{payload.inspect}"
       socket.send_string "Error"
     end
-
-    sleep 1
+    sleep 0.1
   end
 
 end
 
-ad_server = Thread.new do
+discovery_server = Thread.new do
 
   me = "#{last_octet}:#{port}"
 
   context = ZMQ::Context.new
   socket = context.socket(ZMQ::PUB)
-  socket.bind("tcp://0.0.0.0:6665")
+  socket.bind("tcp://*:6665")
 
   while true
-    puts "#{Time.now} [ad] #{me}"
+    puts "#{Time.now} [discovery] > #{me.inspect}"
     socket.send_string me
     sleep 2
   end
 
 end
 
-[hello_server, ad_server].each(&:join)
+[hello_server, discovery_server].each(&:join)
 
